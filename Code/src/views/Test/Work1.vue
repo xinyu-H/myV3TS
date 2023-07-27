960bdf7b2dfcb813d08b0712a5be72fd50b8d3d5
xinyu_H <2431096328@qq.com>
2023-07-27 22:27:06 +0800

    commit

--- Code/src/views/Test/Work1.vue
+++ Code/src/views/Test/Work1.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="box">
         <div class="test">
             <div></div>
             <div></div>
@@ -8,6 +8,41 @@
 </template>
 
 <script setup lang="ts">
+import { nextTick } from "vue";
+
+let scale = 1;
+let boxW;
+const scaleBox = (box: HTMLElement, size: number) => {
+    boxW = box.getBoundingClientRect().width;
+    box.style.transformOrigin = `top left`;
+    while (Math.abs(boxW - size) > 1) {
+        boxW < size ? scale += 0.001 : scale -= 0.001;
+        box.style.transform = `scale(${scale})`;
+        boxW = box.getBoundingClientRect().width;
+    }
+    console.log(boxW, size);
+}
+
+window.addEventListener("resize", () => {
+    scaleBox(document.querySelector(".box") as HTMLElement, window.innerWidth)
+})
+
+nextTick(() => {
+    scaleBox(document.querySelector(".box") as HTMLElement, window.innerWidth);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 function clickLi (e: Event) {
     console.log(e)
 }
@@ -26,11 +61,22 @@ const object = {
 function getVal <T extends object, K extends keyof T> (obj: T, key: K): T[K] {
     return obj[key]
 }
-console.log(getVal(object, 'age'))
+// console.log(getVal(object, 'age'))
 
 </script>
 
 <style scoped lang="scss">
+div {
+    box-sizing: border-box;
+}
+.box {
+    width: 1920px;
+    height: 1080px;
+    overflow: auto;
+    background-color: tan;
+    border: 2px solid red;
+}
+
 .test {
     width: 100%;
     height: 100px;
@@ -38,12 +84,13 @@ console.log(getVal(object, 'age'))
         width: 100px;
         height: 100%;
         float: left;
-        background-color: red;
+        background-color: pink;
     }
     >div:nth-child(2) {
+        width: 50px;
         height: 100%;
         overflow: hidden;
-        background-color: tan;
+        background-color: tomato;
     }
 }
 </style>
